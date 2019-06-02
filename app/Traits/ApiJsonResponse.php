98c3ff6a7c1457ebec8afe724ac0092a8d82e916
Emeka Augustine <cafafanscoders@gamil.com>
2019-06-02 00:16:49 -0400

    Create Traits/ApiJsonResponse

---
+++ app/Traits/ApiJsonResponse.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\Response;
+
+trait ApiJsonResponse
+{
+    /**
+     * Return Success JsonResponse
+     *
+     * @param  string|array $data
+     * @param int $code
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function successResponse($data, $code = Response::HTTP_OK) {
+        return response()->json(['data' => $data], $code);
+    }
+
+    /**
+     * Return Error JsonResponse
+     *
+     * @param  string  $message
+     * @param int $code
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function errorResponse($message, $code) {
+        return response()->json(['error' => $message, 'code' => $code], $code);
+    }
+}
