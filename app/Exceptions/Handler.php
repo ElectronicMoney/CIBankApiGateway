a838d97ccb17b61c0ba79596a680cd6974c8726a
Emeka Augustine <cafafanscoders@gamil.com>
2019-06-02 00:59:01 -0400

    Handle Exceptions in Exceptions/Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -8,9 +8,12 @@ use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Illuminate\Http\Response;
+use App\Traits\ApiJsonResponse;
 
 class Handler extends ExceptionHandler
 {
+    use ApiJsonResponse;
     /**
      * A list of the exception types that should not be reported.
      *
@@ -45,6 +48,38 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        //Check if the exception is an instance of HttpException
+        if( $exception instanceof HttpException) {
+            $code = $exception->getStatusCode();
+            $message = Response::statusTexts[$code];
+            return $this->errorResponse($message, $code);
+        }
+
+        //Check if the exception is an instance of ModelNotFoundException
+        if( $exception instanceof ModelNotFoundException) {
+            $model = strtolower(class_basename($exception->getModel()));
+            $message = "An instance of {$model} with the given id not found";
+            return $this->errorResponse($message, Response::HTTP_NOT_FOUND);
+        }
+
+        //Check if the exception is an instance of AuthorizationException
+        if( $exception instanceof AuthorizationException) {
+            $message = $exception->getMessage();
+            return $this->errorResponse($message, Response::HTTP_FORBIDDEN);
+        }
+
+        //Check if the exception is an instance of ValidationException
+        if( $exception instanceof ValidationException) {
+            $message = $exception->validator->errors()->getMessages();
+            return $this->errorResponse($message, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        //Check if we are in dev mode
+        if (env('APP_DEBUG', false)) {
+            return parent::render($request, $exception);
+        }
+        //If there is no any other error, return inernal server error
+        return $this->errorResponse('Unexpected Error; try again later!', Response::HTTP_INTERNAL_SERVER_ERROR);
     }
+
 }
