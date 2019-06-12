fa35901418d8ec4d79879751b25182d7f4625d94
Emeka Augustine <cafafanscoders@gamil.com>
2019-06-12 01:28:50 -0400

    Modify some file

--- app/Http/Controllers/Authentication/UserController.php
+++ app/Http/Controllers/Authentication/UserController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\User;
+namespace App\Http\Controllers\Authentication;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
--- routes/web.php
+++ routes/web.php
@@ -15,29 +15,38 @@
  * Auhentication routes
  */
 $router->group(['prefix' => 'v1'], function () use ($router) {
-    $router->post('/login', 'Auth\AuthController@login');
-    $router->post('/logout', 'Auth\AuthController@logout');
-    $router->post('/register', 'Auth\AuthController@register');
+    $router->post('/login', 'Authentication\AuthController@login');
+    $router->post('/logout', 'Authentication\AuthController@logout');
+    $router->post('/register', 'Authentication\AuthController@register');
 });
 
 /**
  * Users and Roles routes
  */
-$router->group(['prefix' => 'v1', 'middleware' => 'auth'], function () use ($router) {
+$router->group(['prefix' => 'v1/', 'middleware' => 'auth'], function () use ($router) {
     //UserController routes
-    $router->post('/users', 'User\UserController@store');
-    $router->get('/users', 'User\UserController@index');
-    $router->get('/users/{user}', 'User\UserController@show');
-    $router->put('/users/{user}', 'User\UserController@update');
-    $router->patch('/users/{user}', 'User\UserController@update');
-    $router->delete('/users/{user}', 'User\UserController@destroy');
+    $router->post('users', 'Authentication\UserController@store');
+    $router->get('users', 'Authentication\UserController@index');
+    $router->get('users/{user}', 'Authentication\UserController@show');
+    $router->put('users/{user}', 'Authentication\UserController@update');
+    $router->patch('users/{user}', 'Authentication\UserController@update');
+    $router->delete('users/{user}', 'Authentication\UserController@destroy');
 
     //RoleController routes
-    $router->post('/roles', 'Auth\RoleController@store');
-    $router->get('/roles', 'Auth\RoleController@index');
-    $router->get('/roles/{role}', 'Auth\RoleController@show');
-    $router->put('/roles/{role}', 'Auth\RoleController@update');
-    $router->patch('/roles/{role}', 'Auth\RoleController@update');
-    $router->delete('/roles/{role}', 'Auth\RoleController@destroy');
+    $router->post('roles', 'Authorization\RoleController@store');
+    $router->get('roles', 'Authorization\RoleController@index');
+    $router->get('roles/{role}', 'Authorization\RoleController@show');
+    $router->put('roles/{role}', 'Authorization\RoleController@update');
+    $router->patch('roles/{role}', 'Authorization\RoleController@update');
+    $router->delete('roles/{role}', 'Authorization\RoleController@destroy');
+
+    //ExampleController routes
+    $router->post('examples', 'Services\ExampleServiceController@store');
+    $router->get('examples', 'Services\ExampleServiceController@index');
+    $router->get('examples/{example}', 'Services\ExampleServiceController@show');
+    $router->put('examples/{example}', 'Services\ExampleServiceController@update');
+    $router->patch('examples/{example}', 'Services\ExampleServiceController@update');
+    $router->delete('examples/{example}', 'Services\ExampleServiceController@destroy');
+
 });
 
